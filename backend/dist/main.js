6420c90ccb2ad623c5ccd1b2de08d940ebf198dd
blog <qkrguswl18@naver.com>
2022-04-19 09:29:53 +0900

    frontend 프로젝트 구조 재설정 중

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -6,7 +6,7 @@ const app_module_1 = require("./module/app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe({ transform: true }));
-    await app.listen(3000);
+    await app.listen(3001);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
